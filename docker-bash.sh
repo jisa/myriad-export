3795c08606ef4fce0d5ab80908b7a6ce94579cb0
jirka <jirka@nowhere>
2024-02-19 21:12:05 +0100

    Making it easier to use a plugged-in Movidius NCS2 stick from bash in the docker image.

--- docker-bash.sh
+++ docker-bash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v "${PWD}":/mnt/myriad --entrypoint bash myriad-export
+docker run --rm -it -v "${PWD}":/mnt/myriad -v /dev/bus/usb:/dev/bus/usb --entrypoint bash myriad-export
